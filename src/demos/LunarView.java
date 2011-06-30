895fe64cabac1c1ee8da865168c7c98eeea9106a
jwearnes <jwearnes@mtu.edu>
2011-06-30 14:32:23 -0400

    extended view to draw the ground, a target, and visual representations of a successful or failure-tastic landing.

--- src/demos/LunarView.java
+++ src/demos/LunarView.java
@@ -19,7 +19,10 @@ public class LunarView implements View {
 	private final LanderView landerView;
 	private final Set<Point> stars;
 	private final Polygon mountains;
+	private final Polygon ground;
 	private final VectorFont font;
+
+	private Polygon blast = null;
 
 	public LunarView(LunarWorld model, Controller controller, int width, int height) {
 		this.model      = model;
@@ -39,19 +42,49 @@ public class LunarView implements View {
 			));
 		}
 
+		mountains = buildMountains(width, height, height/2, 20);
+		ground    = buildGround(width, height, (int)model.positions.get(model.target).y);
+	}
+
+	private Polygon buildMountains(int width, int height, int starty, int heightchange) {
 		int[] x = new int[50];
 		int[] y = new int[50];
 		int xp = -10;
-		int yp = height / 2;
+		int yp = starty;
 		for(int i = 1; i < 49; i++) {
 			x[i] = xp;
 			y[i] = yp;
 			xp += (int)(Math.random() * 10) + (width / 40);
-			yp += (int)(Math.random() * 40) - 20;
+			yp += (int)(Math.random() * (2*heightchange)) - heightchange;
 		}
 		x[ 0] = -20;       y[ 0] = 2 * height;
 		x[49] = 2 * width; y[49] = 2 * height;
-		mountains = new Polygon(x, y, 50);
+		return new Polygon(x, y, 50);
+	}
+
+	private Polygon buildGround(int width, int height, int starty) {
+		int[] x = new int[50];
+		int[] y = new int[50];
+		int xp = -10;
+		for(int i = 1; i < 49; i++) {
+			x[i] = xp;
+			y[i] = starty + (int)(Math.random() *  6) - 3;
+			xp +=           (int)(Math.random() * 10) + (width / 40);
+		}
+		x[ 0] = -20;       y[ 0] = 2 * height;
+		x[49] = 2 * width; y[49] = 2 * height;
+		return new Polygon(x, y, 50);
+	}
+
+	private Polygon blast(int sx, int sy) {
+		int[] x = new int[20];
+		int[] y = new int[20];
+		for(int i = 0; i < x.length; i++) {
+			double r = Math.PI * 2 * ((double)i / x.length);
+			x[i] = sx + (int)(Math.cos(r) * ((Math.random() * 20) + 25));
+			y[i] = sy + (int)(Math.sin(r) * ((Math.random() * 20) + 25));
+		}
+		return new Polygon(x, y, 20);
 	}
 
 	public Controller  controller()  { return controller; }
@@ -65,9 +98,6 @@ public class LunarView implements View {
 		g.setColor(colors.background);
 		g.fillRect(0, 0, width, height);
 
-		//g.setColor(colors.outline);
-		//g.drawLine(0, height - 20, width, height - 20);
-
 		g.setColor(colors.outline);
 		for(Point star : stars) {
 			g.drawLine(star.x, star.y, star.x, star.y);
@@ -77,12 +107,49 @@ public class LunarView implements View {
 		g.fillPolygon(mountains);
 		g.setColor(new Color(50, 50, 50));
 		g.drawPolygon(mountains);
+		if (blast != null) {
+			g.setColor(colors.outline);
+			g.fillPolygon(blast);
+			//g.setColor(colors.outline);
+			//g.drawPolygon(blast);
+		}
+		g.setColor(colors.background);
+		g.fillPolygon(ground);
+		g.setColor(colors.outline);
+		g.drawPolygon(ground);
 
 		synchronized(model) {
+
+			AlignedBox bound = model.target.bound();
+			Rectangle target = new Rectangle(
+				(int)bound.point0.x,
+				(int)bound.point0.y,
+				(int)(bound.point1.x - bound.point0.x),
+				(int)(bound.point1.y - bound.point0.y)
+			);
+			g.setColor(colors.background);
+			((Graphics2D)g).fill(target);
+			g.setColor(colors.outline);
+			((Graphics2D)g).draw(target);
+
+			Position lander = model.positions.get(model.lander);
+			landerView.draw(
+				(Graphics2D)g,
+				(int)lander.x,
+				(int)lander.y,
+				model.success()
+			);
+			if (model.done() & !model.success()) {
+				blast = blast(
+					(int)lander.x,
+					(int)lander.y
+				);
+			}
+
 			g.setColor(colors.outline);
 			font.draw(g, 10, 10, String.format("VX:  % 07.2f", model.lander.velocity.x));
 			font.draw(g, 10, 30, String.format("VY:  % 07.2f", model.lander.velocity.y));
-			font.draw(g, 10, 50, String.format("FUEL:% d",    model.lander.fuel));
+			font.draw(g, 10, 50, String.format("FUEL:% 4d",    model.lander.fuel));
 
 			double alt  = model.positions.get(model.target).y - model.positions.get(model.lander).y;
 			double dist = model.positions.get(model.target).x - model.positions.get(model.lander).x;
@@ -90,26 +157,6 @@ public class LunarView implements View {
 			font.draw(g, 10, 70, String.format("ALTITUDE:% 07.2f", alt));
 			font.draw(g, 10, 90, String.format("DISTANCE:% 07.2f", dist));
 
-			/*
-			for(Actor actor : model.actors) {
-				if(actor instanceof Collidable) {
-					AlignedBox bound = ((Collidable)actor).bound();
-					g.drawRect(
-						(int)bound.point0.x,
-						(int)bound.point0.y,
-						(int)(bound.point1.x - bound.point0.x),
-						(int)(bound.point1.y - bound.point0.y)
-					);
-				}
-			}
-			*/
-
-			Position position = model.positions.get(model.lander);
-			landerView.draw(
-				(Graphics2D)g,
-				(int)position.x,
-				(int)position.y
-			);
 		}
 	}
 
@@ -133,7 +180,7 @@ public class LunarView implements View {
 			this.lander = lander;
 		}
 
-		protected void draw(Graphics2D g, int x, int y) {
+		protected void draw(Graphics2D g, int x, int y, boolean success) {
 			g.setColor(Color.WHITE);
 			LunarWorld.ThrusterAction lastThrust = null;
 			
@@ -141,7 +188,6 @@ public class LunarView implements View {
 				java.util.List<Action> actions = controller().previousActions();
 				lastThrust = (LunarWorld.ThrusterAction)Groups.firstOfType(LunarWorld.ThrusterAction.class, actions);
 			}
-			
 			if(lastThrust != null) {
 				if(lastThrust.mThrusterFired) {
 					int  h = (int)(Math.random() * 10) + 20;
@@ -161,6 +207,16 @@ public class LunarView implements View {
 					g.drawLine(x - 16, y - 15, x - 16 - w, y - 13 + dy);
 					g.drawLine(x - 16, y - 11, x - 16 - w, y - 13 + dy);
 				}
+			}
+
+			// victory flag
+			if (success) {
+				Rectangle flag = new Rectangle(x, y - 40, 10, 8);
+				g.setColor(colors.background);
+				g.fill(flag);
+				g.setColor(colors.outline);
+				g.draw(flag);
+				g.drawLine(x, y - 40, x, y);
 			}
 	
 			// base
