7f6f7ac76b86c63b6087e03bace5ba2f30af6d45
Jason Hiebel <jshiebel@mtu.edu>
2011-07-25 15:26:11 -0400

    Changes so that in future versions we might be able to avoid locations in grid annotations!

--- src/vitro/grid/GridAnnotation.java
+++ src/vitro/grid/GridAnnotation.java
@@ -5,13 +5,45 @@ import java.util.*;
 import java.awt.*;
 
 public class GridAnnotation implements Annotation {
-	public final Map<Location, Color> coloring;
+	public final Map<Point, Color> coloring = new HashMap<Point, Color>();
 	
-	public GridAnnotation(Map<Location, Color> coloring) {
-		this.coloring = coloring;
+	
+	public GridAnnotation(Collection<Location> colored, Color color) {
+		for(Location location : colored) { coloring.put(new Point(location.x, location.y), color); }
 	}
 	
+	
+	/*
+	public GridAnnotation(Collection<Point> colored, Color color) {
+		for(Point point : colored) { coloring.put(point, color); }
+	}
+	*/
+	
+	
+	public GridAnnotation(Map<Location, Color> colored) {
+		for(Location location : colored.keySet()) { coloring.put(new Point(location.x, location.y), colored.get(location)); }
+	}
+	
+	
+	/*
+	public GridAnnotation(Map<Point, Color> colored) {
+		for(Point point : colored.keySet()) { coloring.put(point, colored.get(point)); }
+	}
+	*/
+	
+	
 	public GridAnnotation(Map<Location, ? extends Number> scaling, Color colorMin, Color colorMax) {
+		this(calculateScaling(scaling, colorMin, colorMax));
+	}
+	
+	
+	/*
+	public GridAnnotation(Map<Point, ? extends Number> scaling, Color colorMin, Color colorMax) {
+		this(calculateScaling(scaling, colorMin, colorMax));
+	}
+	*/
+	
+	protected static <K> Map<K, Color> calculateScaling(Map<K, ? extends Number> scaling, Color colorMin, Color colorMax) {
 		float min = Float.MAX_VALUE;
 		float max = Float.MIN_VALUE;
 		for(Number num : scaling.values()) {
@@ -22,22 +54,23 @@ public class GridAnnotation implements Annotation {
 		float[] compMin = colorMin.getColorComponents(null);
 		float[] compMax = colorMax.getColorComponents(null);
 		
-		coloring = new HashMap<Location, Color>();
-		for(Location location : scaling.keySet()) {
-		
+		Map<K, Color> colors = new HashMap<K, Color>();
+		for(K key : scaling.keySet()) {
 			if(min != max) {
-				float s = (scaling.get(location).floatValue() - min) / (max - min);
+				float s = (scaling.get(key).floatValue() - min) / (max - min);
 				float[] comp = new float[] {
 					s * (compMax[0] - compMin[0]) + compMin[0],
 					s * (compMax[1] - compMin[1]) + compMin[1],
 					s * (compMax[2] - compMin[2]) + compMin[2]
 				};
 				
-				coloring.put(location, new Color(comp[0], comp[1], comp[2]));
+				colors.put(key, new Color(comp[0], comp[1], comp[2]));
 			}
 			else {
-				coloring.put(location, colorMin);
+				colors.put(key, colorMin);
 			}
 		}
+		
+		return colors;
 	}
 }
