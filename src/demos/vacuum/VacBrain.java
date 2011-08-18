77bf69eb8e5b5e6fdd8d3180ce1686599c522951
Jason Hiebel <jshiebel@mtu.edu>
2011-08-18 11:23:08 -0400

    Added a more interesting and *realistic* map for Scrubby to work on. Fixed a problem where attempting to add an actor to a location where it already exists would cause its removal.

--- src/demos/vacuum/VacBrain.java
+++ src/demos/vacuum/VacBrain.java
@@ -10,18 +10,31 @@ public class VacBrain implements Agent<VacWorld.Scrubby>, Annotated {
 
 	private Actor goalDirt = null;
 	private Set<Edge> visited = new HashSet<Edge>();
-
+	private Set<Edge> notated = new HashSet<Edge>();
+	
 	public Action choose(VacWorld.Scrubby actor, Set<Action> options) {
 
 		// if there's stuff to clean, clean it!
 		DestroyAction clean = actor.destroy(VacWorld.Dirt.class, options);
-		if (clean != null) { return clean; }
+		//System.out.println(clean);
+		if (clean != null) {
+			visited.addAll(notated);
+			notated.clear();
+			//System.out.println();
+			return clean;
+		}
 
 		// otherwise, find stuff to clean!
 		goalDirt = firstOfType(VacWorld.Dirt.class, actor.location().reachableActors());
 		MoveAction move = actor.moveToward(goalDirt, options);
+		
+		//System.out.println(goalDirt);
+		//System.out.println(move);
+		
 		if (move != null) {
-			visited.add(move.edge);
+			//visited.add(move.edge);
+			notated.add(move.edge);
+			//System.out.println();
 			return move;
 		}
 
@@ -31,11 +44,21 @@ public class VacBrain implements Agent<VacWorld.Scrubby>, Annotated {
 
 	public Set<Annotation> annotations() {
 		Set<Annotation> ret = new HashSet<Annotation>();
+		
 		if (goalDirt != null) { ret.add(new ActorAnnotation(goalDirt, "Goal Dirt")); }
+		for(Edge edge : notated) {
+			ret.add(new EdgeAnnotation(edge, "Current Path"));
+		}
+		
+		
+		//if (goalDirt != null) { ret.add(new ActorAnnotation(goalDirt, "Goal Dirt")); }
 		for(Edge edge : visited) {
 			ret.add(new EdgeAnnotation(edge, "Visited Edge"));
 		}
-		ret.add(new DataAnnotation(new HashSet<Edge>(visited), "Visited Edges"));
+
+		//ret.add(new DataAnnotation(new HashSet<Edge>(visited), "Visited Edges"));
+		
+		
 		return ret;
 	}
 	
